461ecc5b032a0a534c02aed10e6f82c998c27460
Bruno Zhong <69879040+Brunozhon@users.noreply.github.com>
2022-04-03 12:51:00 -0400

    Update web-app.js

--- js/web-app.js
+++ js/web-app.js
@@ -20,4 +20,78 @@ class Tile {
   }
 }
 
-// TODO: WorkerPool, PageState, MandelbrotCanvas
+class WorkerPool {
+  constructor(numWorkers, workerSource) {
+    this.idleWorkers = [];
+    this.workQueue = [];
+    this.workerMap = new Map();
+    
+    for (let i = 0; i < numWorkers; i++) {
+      let worker = new Worker(workerSource);
+      worker.onmessage = message => {
+        this._workerDone(worker, null, message.data);
+      }
+      worker.onerror = error => {
+        this._workerDone(worker, error, null);
+      }
+    }
+  }
+  
+  _workerDone(worker, error, response) {
+    let [resolver, rejector] = this.workerMap.get(worker);
+    this.workerMap.delete(worker);
+    if (this.workQueue.length === 0) {
+      this.idleWorkers.push(worker);
+    } else {
+      let [work, resolver, rejector] = this.workQueue.shift();
+      this.workerMap.set(worker, [resolver, rejector]);
+      worker.postMessage(work);
+    }
+    
+    error === null ? resolver(response) : rejector(error);
+  }
+  
+  addWork(work) {
+    return new Promise((resolve, reject) => {
+      if (this.idleWorkers.length > 0) {
+        let worker = this.idleWorkers.pop()
+        this.workerMap.set(worker, [resolve, reject]);
+        worker.postMessage(work);
+      } else {
+        this.workQueue.push([work, resolve, reject]);
+      }
+    });
+  }
+}
+
+class PageState {
+  static initialState() {
+    let s = new PageState();
+    s.cx = -0.5;
+    s.cy = 0;
+    s.perPixel = 3 / window.innerHeight;
+    s.maxIterations = 500;
+    return s;
+  }
+  
+  static fromURL(url) {
+    let s = new PageState();
+    let u = new URL(url);
+    s.cx = parseFloat(u.searchParams.get("cx"))
+    s.cy = parseFloat(u.searchParams.get("cy"))
+    s.perPixel = parseFloat(u.searchParams.get("pp"))
+    s.maxIterations = parseInt(u.searchParams.get("it"))
+    return (isNaN(s.cx) || isNaN(s.cy) || isNaN(s.perPixel) || isNaN(s.maxIterations)) ? null : s;
+  }
+  
+  toURL() {
+    let u = new URL(window.location);
+    u.searchParams.set("cx", this.cx);
+    u.searchParams.set("cy", this.cy);
+    u.searchParams.set("pp", this.perPixel);
+    u.searchParams.set("it", this.maxIterations);
+    return u.href;
+  }
+}
+
+// TODO: MandelbrotCanvas
